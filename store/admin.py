4746142a9c748776a3d8679f606fd2a1b9492b54
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-04 02:15:39 +0600

    Admin Panel

--- store/admin.py
+++ store/admin.py
@@ -2,10 +2,23 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price','inventory_status']
+    list_display = ['title', 'unit_price','inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+
+    def collection_title(self, product):
+        return product.collection.title
+
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -13,11 +26,14 @@ class ProductAdmin(admin.ModelAdmin):
             return 'Low'
         return 'OK'
 
+
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
 
 admin.site.register(models.Customer, CustomerAdmin)
 admin.site.register(models.Product, ProductAdmin)
