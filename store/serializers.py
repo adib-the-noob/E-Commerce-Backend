b2057759467db5616e12e682deb2144bbedfa6c3
ADIB-the-NOOB <mdadib550@gmail.com>
2022-10-23 15:49:08 +0600

    Replacing by Model Serializers

--- store/serializers.py
+++ store/serializers.py
@@ -3,23 +3,27 @@ from decimal import Decimal
 from store.models import Product,Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
 
 
-
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','price_with_tax','collection']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
-    # serializer.PrimaryKeyRelatedField(queryset=Collection.objects.all()) will return the id of the collection
-    # collection = serializers.StringRelatedField()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection_details',
-    )
+    # # serializer.PrimaryKeyRelatedField(queryset=Collection.objects.all()) will return the id of the collection
+    # # collection = serializers.StringRelatedField()
+    
+    
+    # This is for Collection of the products
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Collection.objects.all(),
+    #     view_name = 'collection_details',
+    # )
 
     def get_price_with_tax(self,prouct : Product):
         return prouct.unit_price * Decimal(1.2)
